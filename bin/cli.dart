f479e30bc7663b42bb185148520d3b9463b0cac2
stolpa4 <stolpa413@gmail.com>
2025-01-21 12:56:27 +0300

    Refactoring to the AppArgs struct

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,28 +1,34 @@
 import 'dart:io' as io;
 
-void main(List<String> args) {
-  var verbose = false;
-  var name = 'World';
-  var iterations = 1;
+void main(List<String> prog_args) {
+  final args = AppArgs(prog_args);
 
-  for (var i = 0; i < args.length; i++) {
-    if (args[i] == '-v' || args[i] == '--verbose') {
-      verbose = true;
-    } else if ((args[i] == '-n' || args[i] == '--name') &&
-        i + 1 < args.length) {
-      name = args[i + 1];
-      i++; // Skip next item as it is used as parameter for '--name' option
-    } else if ((args[i] == '-i' || args[i] == '--iterations') &&
-        i + 1 < args.length) {
-      iterations = int.tryParse(args[i + 1]) ?? 1;
-      i++; // Skip next item as it is used as parameter for '--iterations' option
-    }
-  }
-
-  for (var i = 0; i < iterations; i++) {
-    if (verbose) {
+  for (var i = 0; i < args.iterations; i++) {
+    if (args.verbose) {
       print('This is verbose output.');
     }
-    print('Hello, $name!');
+    print('Hello, ${args.name}!');
+  }
+}
+
+class AppArgs {
+  bool verbose = false;
+  String name = 'World';
+  int iterations = 1;
+
+  AppArgs(List<String> args) {
+    for (var i = 0; i < args.length; i++) {
+      if (args[i] == '-v' || args[i] == '--verbose') {
+        verbose = true;
+      } else if ((args[i] == '-n' || args[i] == '--name') &&
+          i + 1 < args.length) {
+        name = args[i + 1];
+        i++;
+      } else if ((args[i] == '-i' || args[i] == '--iterations') &&
+          i + 1 < args.length) {
+        iterations = int.tryParse(args[i + 1]) ?? 1;
+        i++;
+      }
+    }
   }
 }
