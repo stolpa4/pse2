66a210edf79802c7eb0761ce6acc7b541c88f09b
stolpa4 <stolpa413@gmail.com>
2025-01-21 12:48:10 +0300

    Some stub code for the basic arg parsing

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,5 +1,28 @@
-import 'package:cli/cli.dart' as cli;
+import 'dart:io' as io;
 
-void main() {
-  print("Hello, world!");
+void main(List<String> args) {
+  var verbose = false;
+  var name = 'World';
+  var iterations = 1;
+
+  for (var i = 0; i < args.length; i++) {
+    if (args[i] == '-v' || args[i] == '--verbose') {
+      verbose = true;
+    } else if ((args[i] == '-n' || args[i] == '--name') &&
+        i + 1 < args.length) {
+      name = args[i + 1];
+      i++; // Skip next item as it is used as parameter for '--name' option
+    } else if ((args[i] == '-i' || args[i] == '--iterations') &&
+        i + 1 < args.length) {
+      iterations = int.tryParse(args[i + 1]) ?? 1;
+      i++; // Skip next item as it is used as parameter for '--iterations' option
+    }
+  }
+
+  for (var i = 0; i < iterations; i++) {
+    if (verbose) {
+      print('This is verbose output.');
+    }
+    print('Hello, $name!');
+  }
 }
